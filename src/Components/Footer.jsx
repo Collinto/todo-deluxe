0ec496886ab4c34849d63a11e8c0bd6ec6d651eb
Collinto <91027701+Collinto@users.noreply.github.com>
2024-02-03 23:04:05 +0100

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './CSS/Footer.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faMapPin, faPhone, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import {faMapPin, faPhone, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {FaMapPin, FaPhone, FaEnvelope} from 'react-icons/fa';
 
 export default function Footer() {
     return (
@@ -9,9 +10,12 @@ export default function Footer() {
             <div className="contact">
                 <h3>Contact us:</h3>
                 <ul className='addresses'>
-                <li><FontAwesomeIcon icon={faPhone} className='icon'/> +124456789012 </li>
+{/*                 <li><FontAwesomeIcon icon={faPhone} className='icon'/> +124456789012 </li>
                 <li> <FontAwesomeIcon icon={faMapPin} className='icon' style={{marginRight:"25px"}}/> Dallas, Texas, United States</li>
-                <li><FontAwesomeIcon icon={faEnvelope} className="icon"/> <a href="nicodevs@gmail.com">nicodevs@gmail.com </a></li>
+                <li><FontAwesomeIcon icon={faEnvelope} className="icon"/> <a href="nicodevs@gmail.com">nicodevs@gmail.com </a></li> */}
+                <li><FaPhone className='icon'/> +124456789012 </li>
+                <li> <FaMapPin className='icon' style={{marginRight:"25px"}}/> Dallas, Texas, United States</li>
+                <li><FaEnvelope className="icon"/> <a href="nicodevs@gmail.com">nicodevs@gmail.com </a></li>
                 </ul>
             </div>
             <span className="copyright" >&copy; Nico2024</span>
