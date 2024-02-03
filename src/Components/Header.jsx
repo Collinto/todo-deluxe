0a695c98453c5e201e8e0e91a44a2148f73627bd
Collinto <91027701+Collinto@users.noreply.github.com>
2024-02-03 22:54:38 +0100

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,8 @@
 import './CSS/Header.css';
 // import ReactSwitch from 'react-switch'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faComputer } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faComputer } from '@fortawesome/free-solid-svg-icons';
+import {FaComputer} from 'react-icons/fa';
 import {useEffect, useState} from 'react';
 
 export default function Header() {
@@ -21,7 +22,8 @@ export default function Header() {
     },[])
     return (
         <div className="Head">
-            <h2><FontAwesomeIcon className="comp" icon={faComputer}/>NICODEV</h2>
+{/*             <h2><FontAwesomeIcon className="comp" icon={FaComputer}/>NICODEV</h2> */}
+            <h2><FaComputer className="comp" />NICODEV</h2>
             <div className="addons">
                 <div className="date">
                 <h3>{time.toLocaleString()}</h3>                
